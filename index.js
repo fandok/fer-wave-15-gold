071b098556ae19740ff853bd5dfe45bf8d03acd9
fandok <fandika.okdiba@gmail.com>
2023-11-18 10:40:58 +0700

    add new file

---
+++ index.js
@@ -0,0 +1,45 @@
+console.log("makan");
+const lapar = "yes";
+
+// if (lapar === "yes") {
+//   console.log("aku mau makan");
+// } else {
+//   console.log("alhamdulillah");
+// }
+
+lapar === "yes" ? console.log("aku mau makan") : console.log("alhamdulillah");
+
+for (let i = 1; i < 6; i++) {
+  console.log(i + 10);
+}
+
+var diskon = 500;
+if (true) {
+  var diskon = 300;
+}
+
+console.log(diskon);
+
+let diskon2 = 500;
+if (true) {
+  let diskon2 = 300;
+}
+
+console.log(diskon2);
+
+let a = 20;
+let b = "test";
+
+console.log(a + b);
+
+a = "new";
+console.log(a + b);
+
+let name = "Age";
+let age = 17;
+
+if (name === "Budi" && age === 17) {
+  console.log("yes");
+} else {
+  console.log("no");
+}
